eeab3ca175ba343e1088f1c2247bab7bb15504be
Sergey <mma118@yandex.ru>
2023-01-15 15:23:39 +0800

    Added a solution problem3

--- Program.cs
+++ Program.cs
@@ -13,7 +13,44 @@
 // 82 -> 10
 // 9012 -> 12
 
+// System.Console.WriteLine("Введите число: ");
+// int num21 = int.Parse(Console.ReadLine()!);
+// // System.Console.WriteLine($"Введено число {num21}");
+// System.Console.WriteLine($"Сумма всех цифр введенного числа равна: {Summ(num21)} ");
 
+// int Summ(int num21)
+// {
+// int num22 = num21;
+// int count = 0;
+// while (num21 > 0)                                       // Считает количество цифр в числе.
+// {
+//     num21 /= 10;
+//     count += 1;
+// }
+//                                 // System.Console.WriteLine($"В числе {count} цифр.");
+// int result = 0;
+// for (int i = 1; i <= count; i++)                        // Считает сумму всех цифр в числе, начиная с конца.
+// {
+//     int sim = num22%10;
+//     num22 /=10;
+//                                 // System.Console.WriteLine(sim);
+//     result += sim;
+// }
+// return result;
+// }
 
 // Задача 29: Напишите программу, которая задаёт массив из 8 элементов
 // и выводит их на экран. (числа берете любые)
+
+int[] Array = GetArray(8);
+Console.WriteLine($"{String.Join(", ", Array)}");
+
+int[] GetArray(int size)
+{
+    int[] Array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Array[i] = new Random().Next(11);
+    }
+    return Array;
+}
